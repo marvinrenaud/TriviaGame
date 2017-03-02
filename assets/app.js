ab49b03e4c6aa14adb23730465212e2f27a2eed1
marvinrenaud <marvin.renaud@turner.com>
2017-03-02 09:51:33 -0500

    interim draft

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+// Declare the set of global variables to be used
+
+
+// Create countdown timer object
+
+
+// Create arrays to hold Q&A objects
+
+
+
+// Create function to write questions and answer options to the page
+
+
+
+// Create function to capture a click on an answer option
+
+
+
+// Create function to write to top div / main part of page
+
+
+
+// Create funcitons to write to bottom div / lower part of page
